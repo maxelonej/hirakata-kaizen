e13dc77a178a9c6f838467f117a56f8d027f818a
maxelonej <maxelone@yandex.ru>
2024-04-29 21:57:19 +0400

    Add explore

---
+++ Explore.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace HiraKata_Kaizen {
+    partial class Explore {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // Explore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(14)))), ((int)(((byte)(16)))));
+            this.ClientSize = new System.Drawing.Size(650, 400);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Explore";
+            this.Text = "Explore";
+            this.Load += new System.EventHandler(this.Explore_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Explore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HiraKata_Kaizen {
+    public partial class Explore : Form {
+        public Explore() {
+            InitializeComponent();
+        }
+
+        void Explore_Load(object sender, EventArgs e) {
+            ControlBox = false;
+        }
+    }
+}
