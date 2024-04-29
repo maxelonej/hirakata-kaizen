13681613a4100355371be1c5c24ee4a0a9e160e3
maxelonej <maxelone@yandex.ru>
2024-04-29 21:56:20 +0400

    Add achievements

---
+++ Achievements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HiraKata_Kaizen {
+    public partial class Achievements : Form {
+        public Achievements() {
+            InitializeComponent();
+        }
+
+        void Achievements_Load(object sender, EventArgs e) {
+            ControlBox = false;
+        }
+    }
+}
