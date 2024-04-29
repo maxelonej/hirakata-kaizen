2fbdcfabb13f5d345198c4d9dea0d008dcff01be
maxelonej <maxelone@yandex.ru>
2024-04-29 21:56:10 +0400

    Add about

---
+++ About.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace HiraKata_Kaizen {
+    partial class About {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.customPanel1 = new HiraKata_Kaizen.CustomPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.customPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // customPanel1
+            // 
+            this.customPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.customPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(27)))));
+            this.customPanel1.BorderRadius = ((byte)(30));
+            this.customPanel1.Controls.Add(this.label3);
+            this.customPanel1.Controls.Add(this.label1);
+            this.customPanel1.Controls.Add(this.label2);
+            this.customPanel1.ForeColor = System.Drawing.Color.Snow;
+            this.customPanel1.Location = new System.Drawing.Point(66, 135);
+            this.customPanel1.Name = "customPanel1";
+            this.customPanel1.Size = new System.Drawing.Size(518, 130);
+            this.customPanel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.label3.Location = new System.Drawing.Point(28, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(384, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "обеспечивающее всестороннее изучение ";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.label1.Location = new System.Drawing.Point(28, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hiragana Katakana Kaizen - десктопное приложение";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.label2.Location = new System.Drawing.Point(28, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "хираганы и катаканы";
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(14)))), ((int)(((byte)(16)))));
+            this.ClientSize = new System.Drawing.Size(650, 400);
+            this.Controls.Add(this.customPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "About";
+            this.Text = "About";
+            this.Load += new System.EventHandler(this.About_Load);
+            this.customPanel1.ResumeLayout(false);
+            this.customPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomPanel customPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ About.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HiraKata_Kaizen {
+    public partial class About : Form {
+        public About() {
+            InitializeComponent();
+        }
+
+        void About_Load(object sender, EventArgs e) {
+            ControlBox = false;
+        }
+    }
+}
