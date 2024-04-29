eb7abbce8718587d4110ebef320c619495e06fb1
maxelonej <maxelone@yandex.ru>
2024-04-29 21:55:58 +0400

    Add checkbox imgs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace HiraKata_Kaizen.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap checkedCheckbox {
+            get {
+                object obj = ResourceManager.GetObject("checkedCheckbox", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap uncheckedCheckbox {
+            get {
+                object obj = ResourceManager.GetObject("uncheckedCheckbox", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
