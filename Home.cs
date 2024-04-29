161637d3bace7135891fe56c87719064a83bf4ee
maxelonej <maxelone@yandex.ru>
2024-04-29 21:57:29 +0400

    Add home

---
+++ Home.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HiraKata_Kaizen {
+    public partial class Home : Form {
+        public Home() {
+            InitializeComponent();
+        }
+
+        void Home_Load(object sender, EventArgs e) {
+            ControlBox = false;
+        }
+    }
+}
