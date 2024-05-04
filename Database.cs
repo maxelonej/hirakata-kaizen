6cc487250284300b0962a1c47a97dfa754a2c84b
maxelonej <maxelone@yandex.ru>
2024-05-04 15:33:29 +0400

    Init db

---
+++ Database.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace HiraKata_Kaizen {
+    internal class Database {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=hirakata_kaizen;Integrated Security=True");
+
+        public void openConnection() {
+            if (con.State == System.Data.ConnectionState.Closed) {
+                con.Open();
+            }
+        }
+        public void closeConnection() {
+            if (con.State == System.Data.ConnectionState.Open) {
+                con.Close();
+            }
+        }
+
+        public SqlConnection getConnection() {
+            return con;
+        }
+    }
+}
