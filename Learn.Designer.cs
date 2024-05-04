b86b94b0be819bd0e58c579995f8541b8d38ef8d
maxelonej <maxelone@yandex.ru>
2024-05-04 15:29:28 +0400

    Make clear form

--- Learn.Designer.cs
+++ Learn.Designer.cs
@@ -23,14 +23,35 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.btnQuiz = new HiraKata_Kaizen.CustomButton();
             this.SuspendLayout();
+            // 
+            // btnQuiz
+            // 
+            this.btnQuiz.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
+            this.btnQuiz.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
+            this.btnQuiz.BorderColor = System.Drawing.Color.Transparent;
+            this.btnQuiz.BorderRadius = 10;
+            this.btnQuiz.BorderSize = 0;
+            this.btnQuiz.FlatAppearance.BorderSize = 0;
+            this.btnQuiz.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuiz.ForeColor = System.Drawing.Color.White;
+            this.btnQuiz.Location = new System.Drawing.Point(432, 209);
+            this.btnQuiz.Name = "btnQuiz";
+            this.btnQuiz.Size = new System.Drawing.Size(150, 50);
+            this.btnQuiz.TabIndex = 2;
+            this.btnQuiz.Text = "Викторина";
+            this.btnQuiz.TextColor = System.Drawing.Color.White;
+            this.btnQuiz.UseVisualStyleBackColor = false;
+            this.btnQuiz.Click += new System.EventHandler(this.btnQuiz_Click);
             // 
             // Learn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(14)))), ((int)(((byte)(16)))));
-            this.ClientSize = new System.Drawing.Size(634, 361);
+            this.ClientSize = new System.Drawing.Size(1015, 469);
+            this.Controls.Add(this.btnQuiz);
             this.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -43,5 +64,6 @@
         }
 
         #endregion
+        private CustomButton btnQuiz;
     }
 }
--- Learn.cs
+++ Learn.cs
@@ -1,21 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HiraKata_Kaizen {
     public partial class Learn : Form {
+
         public Learn() {
             InitializeComponent();
         }
 
         void Learn_Load(object sender, EventArgs e) {
             ControlBox = false;
+        }
+
+        void btnQuiz_Click(object sender, EventArgs e) {
+            var dashboard = Application.OpenForms.OfType<Dashboard>().FirstOrDefault();
+            if (dashboard.content.Controls.Count > 0) dashboard.content.Controls.RemoveAt(0);
+            Quiz quiz = new Quiz();
+            quiz.TopLevel = false;
+            quiz.Dock = DockStyle.Fill;
+            dashboard.content.Controls.Add(quiz);
+            dashboard.content.Tag = quiz;
+            quiz.Show();
         }
     }
 }
