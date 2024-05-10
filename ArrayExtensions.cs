bf197817679473a092a90f3486617e1c718253e6
maxelonej <maxelone@yandex.ru>
2024-05-10 11:27:27 +0400

    Add array.Shuffle

---
+++ ArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HiraKata_Kaizen {
+    public static class ArrayExtensions {
+        private static Random _random = new Random();
+
+        public static void Shuffle<T>(this T[] array) {
+            int n = array.Length; // len array
+            while (n > 1) { // from the last element to the first
+                int k = _random.Next(n--); // generate a random index k such that 0 <= k < n
+                T temp = array[n - 1]; // use n - 1 because arrays are 0-based
+                array[n - 1] = array[k];
+                array[k] = temp;
+
+                n--; // Decrement n to move to the next iteration
+            }
+        }
+    }
+}
