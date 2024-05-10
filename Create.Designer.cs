138fd0ae70d9b111c834bfbf15ee062964169759
maxelonej <maxelone@yandex.ru>
2024-05-10 11:08:34 +0400

    Init Create.cs

---
+++ Create.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace HiraKata_Kaizen {
+    partial class Create {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.customPanel1 = new HiraKata_Kaizen.CustomPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.customPanel2 = new HiraKata_Kaizen.CustomPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.customPanel4 = new HiraKata_Kaizen.CustomPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.customPanel5 = new HiraKata_Kaizen.CustomPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.customPanel6 = new HiraKata_Kaizen.CustomPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.customButton1 = new HiraKata_Kaizen.CustomButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.customPanel3 = new HiraKata_Kaizen.CustomPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.customPanel7 = new HiraKata_Kaizen.CustomPanel();
+            this.customPanel1.SuspendLayout();
+            this.customPanel2.SuspendLayout();
+            this.customPanel4.SuspendLayout();
+            this.customPanel5.SuspendLayout();
+            this.customPanel6.SuspendLayout();
+            this.customPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // customPanel1
+            // 
+            this.customPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
+            this.customPanel1.BorderRadius = ((byte)(30));
+            this.customPanel1.Controls.Add(this.label1);
+            this.customPanel1.ForeColor = System.Drawing.Color.Snow;
+            this.customPanel1.Location = new System.Drawing.Point(12, 82);
+            this.customPanel1.Name = "customPanel1";
+            this.customPanel1.Size = new System.Drawing.Size(269, 54);
+            this.customPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Время";
+            // 
+            // customPanel2
+            // 
+            this.customPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
+            this.customPanel2.BorderRadius = ((byte)(30));
+            this.customPanel2.Controls.Add(this.label2);
+            this.customPanel2.ForeColor = System.Drawing.Color.Snow;
+            this.customPanel2.Location = new System.Drawing.Point(12, 152);
+            this.customPanel2.Name = "customPanel2";
+            this.customPanel2.Size = new System.Drawing.Size(269, 54);
+            this.customPanel2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Количество";
+            // 
+            // customPanel4
+            // 
+            this.customPanel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
+            this.customPanel4.BorderRadius = ((byte)(30));
+            this.customPanel4.Controls.Add(this.label5);
+            this.customPanel4.ForeColor = System.Drawing.Color.Snow;
+            this.customPanel4.Location = new System.Drawing.Point(12, 290);
+            this.customPanel4.Name = "customPanel4";
+            this.customPanel4.Size = new System.Drawing.Size(269, 54);
+            this.customPanel4.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 21);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Ответы";
+            // 
+            // customPanel5
+            // 
+            this.customPanel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
+            this.customPanel5.BorderRadius = ((byte)(30));
+            this.customPanel5.Controls.Add(this.label6);
+            this.customPanel5.ForeColor = System.Drawing.Color.Snow;
+            this.customPanel5.Location = new System.Drawing.Point(12, 12);
+            this.customPanel5.Name = "customPanel5";
+            this.customPanel5.Size = new System.Drawing.Size(269, 54);
+            this.customPanel5.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 21);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Категория";
+            // 
+            // customPanel6
+            // 
+            this.customPanel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
+            this.customPanel6.BorderRadius = ((byte)(30));
+            this.customPanel6.Controls.Add(this.label3);
+            this.customPanel6.ForeColor = System.Drawing.Color.Snow;
+            this.customPanel6.Location = new System.Drawing.Point(310, 342);
+            this.customPanel6.Name = "customPanel6";
+            this.customPanel6.Size = new System.Drawing.Size(370, 60);
+            this.customPanel6.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Выбраны для повторения:";
+            // 
+            // customButton1
+            // 
+            this.customButton1.BackColor = System.Drawing.Color.Maroon;
+            this.customButton1.BackgroundColor = System.Drawing.Color.Maroon;
+            this.customButton1.BorderColor = System.Drawing.Color.Transparent;
+            this.customButton1.BorderRadius = 10;
+            this.customButton1.BorderSize = 0;
+            this.customButton1.FlatAppearance.BorderSize = 0;
+            this.customButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customButton1.ForeColor = System.Drawing.Color.White;
+            this.customButton1.Location = new System.Drawing.Point(821, 352);
+            this.customButton1.Name = "customButton1";
+            this.customButton1.Size = new System.Drawing.Size(150, 40);
+            this.customButton1.TabIndex = 3;
+            this.customButton1.Text = "Сохранить";
+            this.customButton1.TextColor = System.Drawing.Color.White;
+            this.customButton1.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 360);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 42);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "сделать возможность\r\nне выбирать";
+            // 
+            // customPanel3
+            // 
+            this.customPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
+            this.customPanel3.BorderRadius = ((byte)(30));
+            this.customPanel3.Controls.Add(this.label7);
+            this.customPanel3.ForeColor = System.Drawing.Color.Snow;
+            this.customPanel3.Location = new System.Drawing.Point(12, 222);
+            this.customPanel3.Name = "customPanel3";
+            this.customPanel3.Size = new System.Drawing.Size(269, 54);
+            this.customPanel3.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 21);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Вопросы";
+            // 
+            // customPanel7
+            // 
+            this.customPanel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
+            this.customPanel7.BorderRadius = ((byte)(30));
+            this.customPanel7.ForeColor = System.Drawing.Color.Snow;
+            this.customPanel7.Location = new System.Drawing.Point(310, 13);
+            this.customPanel7.Name = "customPanel7";
+            this.customPanel7.Size = new System.Drawing.Size(661, 315);
+            this.customPanel7.TabIndex = 5;
+            // 
+            // Create
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(999, 430);
+            this.Controls.Add(this.customPanel7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.customButton1);
+            this.Controls.Add(this.customPanel6);
+            this.Controls.Add(this.customPanel3);
+            this.Controls.Add(this.customPanel5);
+            this.Controls.Add(this.customPanel4);
+            this.Controls.Add(this.customPanel2);
+            this.Controls.Add(this.customPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Create";
+            this.Text = "Create";
+            this.customPanel1.ResumeLayout(false);
+            this.customPanel1.PerformLayout();
+            this.customPanel2.ResumeLayout(false);
+            this.customPanel2.PerformLayout();
+            this.customPanel4.ResumeLayout(false);
+            this.customPanel4.PerformLayout();
+            this.customPanel5.ResumeLayout(false);
+            this.customPanel5.PerformLayout();
+            this.customPanel6.ResumeLayout(false);
+            this.customPanel6.PerformLayout();
+            this.customPanel3.ResumeLayout(false);
+            this.customPanel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CustomPanel customPanel1;
+        private System.Windows.Forms.Label label1;
+        private CustomPanel customPanel2;
+        private System.Windows.Forms.Label label2;
+        private CustomPanel customPanel4;
+        private System.Windows.Forms.Label label5;
+        private CustomPanel customPanel5;
+        private System.Windows.Forms.Label label6;
+        private CustomPanel customPanel6;
+        private System.Windows.Forms.Label label3;
+        private CustomButton customButton1;
+        private System.Windows.Forms.Label label4;
+        private CustomPanel customPanel3;
+        private System.Windows.Forms.Label label7;
+        private CustomPanel customPanel7;
+    }
+}
