3ff8da27be8fb1cb26340badb44a61d345995d38
maxelonej <maxelone@yandex.ru>
2024-05-11 15:55:56 +0400

    Remove panel, move panel

--- Create.Designer.cs
+++ Create.Designer.cs
@@ -31,8 +31,6 @@
             this.label5 = new System.Windows.Forms.Label();
             this.customPanel5 = new HiraKata_Kaizen.CustomPanel();
             this.label6 = new System.Windows.Forms.Label();
-            this.customPanel6 = new HiraKata_Kaizen.CustomPanel();
-            this.label3 = new System.Windows.Forms.Label();
             this.customButton1 = new HiraKata_Kaizen.CustomButton();
             this.label4 = new System.Windows.Forms.Label();
             this.customPanel3 = new HiraKata_Kaizen.CustomPanel();
@@ -42,8 +40,8 @@
             this.customPanel2.SuspendLayout();
             this.customPanel4.SuspendLayout();
             this.customPanel5.SuspendLayout();
-            this.customPanel6.SuspendLayout();
             this.customPanel3.SuspendLayout();
+            this.customPanel7.SuspendLayout();
             this.SuspendLayout();
             // 
             // customPanel1
@@ -52,7 +50,7 @@
             this.customPanel1.BorderRadius = ((byte)(30));
             this.customPanel1.Controls.Add(this.label1);
             this.customPanel1.ForeColor = System.Drawing.Color.Snow;
-            this.customPanel1.Location = new System.Drawing.Point(12, 82);
+            this.customPanel1.Location = new System.Drawing.Point(12, 86);
             this.customPanel1.Name = "customPanel1";
             this.customPanel1.Size = new System.Drawing.Size(269, 54);
             this.customPanel1.TabIndex = 0;
@@ -72,7 +70,7 @@
             this.customPanel2.BorderRadius = ((byte)(30));
             this.customPanel2.Controls.Add(this.label2);
             this.customPanel2.ForeColor = System.Drawing.Color.Snow;
-            this.customPanel2.Location = new System.Drawing.Point(12, 152);
+            this.customPanel2.Location = new System.Drawing.Point(12, 159);
             this.customPanel2.Name = "customPanel2";
             this.customPanel2.Size = new System.Drawing.Size(269, 54);
             this.customPanel2.TabIndex = 0;
@@ -92,7 +90,7 @@
             this.customPanel4.BorderRadius = ((byte)(30));
             this.customPanel4.Controls.Add(this.label5);
             this.customPanel4.ForeColor = System.Drawing.Color.Snow;
-            this.customPanel4.Location = new System.Drawing.Point(12, 290);
+            this.customPanel4.Location = new System.Drawing.Point(12, 305);
             this.customPanel4.Name = "customPanel4";
             this.customPanel4.Size = new System.Drawing.Size(269, 54);
             this.customPanel4.TabIndex = 0;
@@ -112,7 +110,7 @@
             this.customPanel5.BorderRadius = ((byte)(30));
             this.customPanel5.Controls.Add(this.label6);
             this.customPanel5.ForeColor = System.Drawing.Color.Snow;
-            this.customPanel5.Location = new System.Drawing.Point(12, 12);
+            this.customPanel5.Location = new System.Drawing.Point(12, 13);
             this.customPanel5.Name = "customPanel5";
             this.customPanel5.Size = new System.Drawing.Size(269, 54);
             this.customPanel5.TabIndex = 1;
@@ -126,26 +124,6 @@
             this.label6.TabIndex = 0;
             this.label6.Text = "Категория";
             // 
-            // customPanel6
-            // 
-            this.customPanel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
-            this.customPanel6.BorderRadius = ((byte)(30));
-            this.customPanel6.Controls.Add(this.label3);
-            this.customPanel6.ForeColor = System.Drawing.Color.Snow;
-            this.customPanel6.Location = new System.Drawing.Point(310, 342);
-            this.customPanel6.Name = "customPanel6";
-            this.customPanel6.Size = new System.Drawing.Size(370, 60);
-            this.customPanel6.TabIndex = 1;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 20);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(197, 21);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Выбраны для повторения:";
-            // 
             // customButton1
             // 
             this.customButton1.BackColor = System.Drawing.Color.Maroon;
@@ -156,7 +134,7 @@
             this.customButton1.FlatAppearance.BorderSize = 0;
             this.customButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.customButton1.ForeColor = System.Drawing.Color.White;
-            this.customButton1.Location = new System.Drawing.Point(821, 352);
+            this.customButton1.Location = new System.Drawing.Point(69, 378);
             this.customButton1.Name = "customButton1";
             this.customButton1.Size = new System.Drawing.Size(150, 40);
             this.customButton1.TabIndex = 3;
@@ -167,7 +145,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(43, 360);
+            this.label4.Location = new System.Drawing.Point(147, 151);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(165, 42);
             this.label4.TabIndex = 4;
@@ -179,7 +157,7 @@
             this.customPanel3.BorderRadius = ((byte)(30));
             this.customPanel3.Controls.Add(this.label7);
             this.customPanel3.ForeColor = System.Drawing.Color.Snow;
-            this.customPanel3.Location = new System.Drawing.Point(12, 222);
+            this.customPanel3.Location = new System.Drawing.Point(12, 232);
             this.customPanel3.Name = "customPanel3";
             this.customPanel3.Size = new System.Drawing.Size(269, 54);
             this.customPanel3.TabIndex = 1;
@@ -197,10 +175,11 @@
             // 
             this.customPanel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(34)))));
             this.customPanel7.BorderRadius = ((byte)(30));
+            this.customPanel7.Controls.Add(this.label4);
             this.customPanel7.ForeColor = System.Drawing.Color.Snow;
-            this.customPanel7.Location = new System.Drawing.Point(310, 13);
+            this.customPanel7.Location = new System.Drawing.Point(299, 13);
             this.customPanel7.Name = "customPanel7";
-            this.customPanel7.Size = new System.Drawing.Size(661, 315);
+            this.customPanel7.Size = new System.Drawing.Size(688, 405);
             this.customPanel7.TabIndex = 5;
             // 
             // Create
@@ -210,9 +189,7 @@
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(999, 430);
             this.Controls.Add(this.customPanel7);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.customButton1);
-            this.Controls.Add(this.customPanel6);
             this.Controls.Add(this.customPanel3);
             this.Controls.Add(this.customPanel5);
             this.Controls.Add(this.customPanel4);
@@ -232,12 +209,11 @@
             this.customPanel4.PerformLayout();
             this.customPanel5.ResumeLayout(false);
             this.customPanel5.PerformLayout();
-            this.customPanel6.ResumeLayout(false);
-            this.customPanel6.PerformLayout();
             this.customPanel3.ResumeLayout(false);
             this.customPanel3.PerformLayout();
+            this.customPanel7.ResumeLayout(false);
+            this.customPanel7.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -251,8 +227,6 @@
         private System.Windows.Forms.Label label5;
         private CustomPanel customPanel5;
         private System.Windows.Forms.Label label6;
-        private CustomPanel customPanel6;
-        private System.Windows.Forms.Label label3;
         private CustomButton customButton1;
         private System.Windows.Forms.Label label4;
         private CustomPanel customPanel3;
